2cbcb480027ff378828ef5d7cd4895746bfc5b0e
Dominic DeRosa <dderosa71@gmail.com>
2021-07-14 11:02:40 -0500

    commit tree 1e17a1884fba3581e80cd411e7f7e4c626c86ad1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(x="roller-skate"){
+    return `This Saturday, I want to ${x}!` 
+}
+
+const mondayWork = function(x='go to the office'){
+    return `This Monday, I will ${x}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(x='special'){
+        return `You are ${flair}${x}${flair}!`
+
+    }
+}
